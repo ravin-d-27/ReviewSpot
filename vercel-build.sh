42204322f1b2e4cef1635227762c88f91bcb354f
Ravin D <ravin.d3107@outlook.com>
2024-10-28 23:13:39 +0530

    Made changes in vercel-build

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "########################################## Checking Python Version ##########################################"
-python3 --version  # This will help confirm that the correct version is used
-
-echo "########################################## Upgrading pip, setuptools, and wheel ##########################################"
-python3 -m pip install --upgrade pip setuptools wheel
+python3.9 --version  # This will help confirm that the correct version is used
 
 echo "########################################## Installing dependencies ##########################################"
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
