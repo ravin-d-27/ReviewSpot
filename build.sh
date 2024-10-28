20fcbe1818a60d329fc853c78e0bc0e0f55c7780
Ravin D <ravin.d3107@outlook.com>
2024-10-28 19:46:02 +0530

    Vercel Deployment

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-echo "########################################## Setting up Virtual Environment ##########################################"
-python3.9 -m venv env
+echo "Setting up Virtual Environment..."
+python3.11 -m venv env
 source env/bin/activate
 
-echo "########################################## Upgrading pip, setuptools, and wheel ##########################################"
-python3.9 -m pip install --upgrade pip setuptools wheel
+echo "Upgrading pip, setuptools, and wheel..."
+python3.11 -m pip install --upgrade pip setuptools wheel
 
-echo "########################################## Installing dependencies ##########################################"
-python3.9 -m pip install -r requirements.txt
+echo "Installing dependencies..."
+python3.11 -m pip install -r requirements.txt
 
-echo "########################################## Running Migrations ##########################################"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+echo "Running Migrations..."
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
 
-echo "########################################## Collecting Static Files ##########################################"
-python3.9 manage.py collectstatic --noinput --clear
+echo "Collecting Static Files..."
+python3.11 manage.py collectstatic --noinput --clear
