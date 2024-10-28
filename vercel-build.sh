a6ea397342a568cd7ba9ae3f265266d5933bbc75
Ravin D <ravin.d3107@outlook.com>
2024-10-28 23:16:10 +0530

    Made changes in vercel-build

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Update package lists
+apt-get update
+
+# Install curl if not available (might not be necessary in Vercel)
+apt-get install -y curl
+
+# Download and install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+
 echo "########################################## Checking Python Version ##########################################"
 python3.9 --version  # This will help confirm that the correct version is used
 
