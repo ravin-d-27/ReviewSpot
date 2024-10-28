b39c45f6ca6632cb9b46a8ffbf3a79b30ead6d12
Ravin D <ravin.d3107@outlook.com>
2024-10-28 19:52:40 +0530

    Settings.py mistake

--- ReviewSpot/settings.py
+++ ReviewSpot/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'File_Server/static/')
+    os.path.join(BASE_DIR, 'ReviewSpot/static/')
 ]
 
 STATIC_URL = '/static/'
