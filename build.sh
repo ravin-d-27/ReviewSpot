55008324981f19ff704c1a32cc5f9e425578e4ae
Ravin D <ravin.d3107@outlook.com>
2024-10-28 20:30:49 +0530

    Trying in 3.9

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+
+echo "########################################## Setting up Virtual Environment ##########################################"
+python3.9 -m venv env
+source env/bin/activate
+
+echo "########################################## Checking Python Version ##########################################"
+python --version  # This will help confirm that the correct version is used
+
 echo "########################################## Upgrading pip, setuptools, and wheel ##########################################"
-python3.9 -m pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade pip setuptools wheel
 
 echo "########################################## Installing dependencies ##########################################"
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "########################################## Running Migrations ##########################################"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
-echo "Collecting Static Files..."
-python3.9 manage.py collectstatic --noinput --clear
+echo "########################################## Collecting Static Files ##########################################"
+python manage.py collectstatic --noinput --clear
