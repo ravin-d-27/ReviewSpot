bceb182338d8d3db2ea1ff2aca0187296f1548e4
Ravin D <ravin.d3107@outlook.com>
2024-10-28 19:55:43 +0530

    Trying in 3.9

--- build.sh
+++ build.sh
@@ -11,6 +11,3 @@ python3.9 -m pip install -r requirements.txt
 echo "########################################## Running Migrations ##########################################"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-echo "########################################## Collecting Static Files ##########################################"
-python3.9 manage.py collectstatic --noinput --clear
