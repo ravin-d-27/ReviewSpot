d4564e160784cef2cc782ac98fa86ab1c579de21
Ravin D <ravin.d3107@outlook.com>
2024-10-28 19:35:02 +0530

    Vercel Deployment

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
-echo "Setting up Virtual Environment..."
-python3.9 -m venv env
+echo "########################################## Setting up Virtual Environment ##########################################"
+python3.12 -m venv env
 source env/bin/activate
 
-echo "Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
+echo "########################################## Upgrading pip, setuptools, and wheel ##########################################"
+python3.12 -m pip install --upgrade pip setuptools wheel
 
-echo "Running Migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+echo "########################################## Installing dependencies ##########################################"
+python3.12 -m pip install -r requirements.txt
 
-echo "Collecting Static Files..."
-python3.9 manage.py collectstatic --noinput --clear
+echo "########################################## Running Migrations ##########################################"
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+
+echo "########################################## Collecting Static Files ##########################################"
+python3.12 manage.py collectstatic --noinput --clear
